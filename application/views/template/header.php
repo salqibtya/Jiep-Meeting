b6f2aa41124a87bafbdeb737439063386489457f
Sarah Al Qibthiyyah <sqibtya@yahoo.com>
2017-07-19 10:38:47 +0700

    take it back

---
+++ application/views/template/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title><?=APP_NAME?> | <?=$title?></title>
+
+	<!-- load css -->
+	<?php $this->load->view('templates/css'); ?>
+	<!-- /load css -->
+</head>
+
+
+<body class="nav-md">
+<div class="col-md-6">
+	<h1>bibimbap</h1>
+</div>
+<div class="container body">
+	<div class="main_container">
+
+		<!-- topbar -->
+		<?php $this->load->view('templates/topbar'); ?>
+		<!-- /topbar -->
