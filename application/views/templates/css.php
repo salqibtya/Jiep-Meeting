61e57aa39fc682fa6be7b58f3adaec6099387573
mukhibillah <mukhib.cascuser303@gmail.com>
2017-07-24 07:51:24 +0700

    take modal

--- application/views/templates/css.php
+++ application/views/templates/css.php
@@ -1,6 +1,6 @@
 <!-- basic -->
-<link href="<?=('./assets/css/')?>bootstrap.min.css" rel="stylesheet">
-<link href="<?=('./assets/css/')?>font-awesome.min.css" rel="stylesheet">
-<link href="<?=('./assets/css/')?>nprogress.css" rel="stylesheet">
-<link href="<?=('./assets/css/')?>custom.min.css" rel="stylesheet">
+<link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="./assets/css/nprogress.css" rel="stylesheet">
+<link href="./assets/css/custom.min.css" rel="stylesheet">
 <!-- /basic -->
