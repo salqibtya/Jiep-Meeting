65ed2458b7061383b3e6c48960537df5f40fd83b
mukhibillah <mukhib.cascuser303@gmail.com>
2017-07-25 09:22:12 +0700

    fix some controller Divisi

--- application/models/Meeting_model.php
+++ application/models/Meeting_model.php
@@ -18,4 +18,15 @@ class Meeting_model extends CI_Model{
 		$this->db->where('tanggal >= CURDATE()');
 		return $this->db->get('meeting')->result_array();
 	}
+
+	function get_one_divisi(){
+		$id_divisi = $this->session->userdata('id_user');
+		$this->db->order_by('tanggal', 'asc');
+		$this->db->order_by('waktu_mulai', 'asc');
+		$this->db->join('ruangan', 'id_ruangan = ruangan_meeting');
+		$this->db->join('divisi','id_divisi = divisi_meeting');
+		$array=array('tanggal >= CURDATE()','id_divisi'=>$id_divisi);
+		$this->db->where($array);
+		return $this->db->get('meeting')->result_array();
+	}
 }
--- application/views/divisiview.php
+++ application/views/divisiview.php
@@ -1,83 +1,43 @@
-<!-- admin content -->
-<body style="background-color:rgb(247,247,247) ">
-<div class="right_col" role="main" style="margin-left:0px!important">
-  <div class="page-title">
-   <div class="title-right" style="background-color: rgb(247,247,247)">   
-   <?php $this->load->view('templates/menubar'); ?>
-
-   <div class="row">
-   <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
-     <div class="x_content">
-      <div class="col-md">
-       <div class="table-responsive">
-        <table class="table table-striped table-bordered jambo_table bulk_act
-              ion">
-         <thead>
-          <tr class="headings">
-            <th style="text-align:center">Tanggal</th>
-            <th style="text-align:center">Jam Mulai</th>
-            <th style="text-align:center">Jam Selesai</th>
-            <th style="text-align:center">Ruangan</th>
-            <th style="text-align:center">Perihal</th> 
-            <th style="text-align:center">Pengguna</th> 
-            <th style="text-align:center;width:100px">
-              <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"> Tambah</i></button>
-              
-              <!-- Modal -->
-              <div class="modal fade" id="myModal" role="dialog">
-              <div class="modal-dialog modal-md">
-                <form>
-                  <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" style="color: black">Tambah Ruangan</h4>
-                  </div>
-                  
-                  <div class="modal-body">
-                    <label class="col-sm-2  control-label" for="sm" style="color: black">Ruangan</label>
-                    <input class="form-control" type="text" id="sm" style="color: black">
-                    <label class="col-sm-2  control-label" for="sm" style="color: black">Kapasitas</label>
-                    <input class="form-control" type="number" min="0" id="sm" style="color: black">
-                  </div>
-               
-                  <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary" data-dismiss="modal">Tambah</button>
-                  </div>
-                  </div>
-                </form>
+<!-- home content -->
+<body style="background-color:rgb(247,247,247) !important ">
+<div class="container body" style="top: 0; bottom: 0; left: 0; height: 100%">
+ <div class="main_container">
+ <?php $this->load->view('templates/menubar'); ?>
+  <div class="right_col" role="main" style="margin-left:0px !important">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered jambo_table bulk_action">
+                <thead>
+                  <tr class="headings">
+                    <th style="text-align:center">Tanggal</th>
+                    <th style="text-align:center">Jam Mulai</th>
+                    <th style="text-align:center">Jam Selesai</th>
+                    <th style="text-align:center">Ruangan</th>
+                    <th style="text-align:center">Perihal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach($meetings as $meetings_item):?>
+                    <tr>
+                      <td style="text-align:center"><?=$meetings_item['tanggal']?></td>
+                      <td style="text-align:center"><?=$meetings_item['waktu_mulai']?></td>
+                      <td style="text-align:center"><?=$meetings_item['waktu_selesai']?></td>
+                      <td style="text-align:center"><?=$meetings_item['nama_ruangan']?></td>
+                      <td style="text-align:center;"><?=$meetings_item['perihal']?></td>
+                    </tr>
+                    <?php endforeach?>
+                  </tbody>
+                 </table>
+                 </div>
               </div>
-              </div>
+            </div>
+          </div>
         </div>
-           </th>
-          </tr>
-         </thead>
-
-        <tbody>
-          <tr>
-            <td style="text-align:center">17 Juni 2017</td>
-            <td style="text-align:center">08.0</td>
-            <td style="text-align:center">12.00</td>
-            <td style="text-align:center">Meeting Room 1</td>
-            <td style="text-align:center;">Redesign Sistem</td>
-            <td style="text-align:center">Information Technology</td>
-            <td style="text-align:center;width:100px">
-              <a href="#" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
-              <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i></a>
-            </td>
-          </tr>
-        </tbody>
-        </table>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-        <div class="clearfix"></div>
-
       </div>
     </div>
   </div>
 </body>
+<!-- home content -->
