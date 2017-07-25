6d4a1afb60b640cd7b24bacd826387969a738f0d
Sarah Al Qibthiyyah <sqibtya@yahoo.com>
2017-07-25 16:44:44 +0700

    add views

--- application/views/meetingdivisiview.php
+++ application/views/meetingdivisiview.php
@@ -8,6 +8,7 @@
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_content">
+          
             <div class="table-responsive">
               <table class="table table-striped table-bordered jambo_table bulk_action">
                 <thead>
