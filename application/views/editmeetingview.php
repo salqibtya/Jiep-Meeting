23329b9c94adbb154c8f417abf8b373bdc51a6f7
mukhibillah <mukhib.cascuser303@gmail.com>
2017-07-31 14:35:22 +0700

    fix the edit meeting

--- application/views/editmeetingview.php
+++ application/views/editmeetingview.php
@@ -26,17 +26,18 @@
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Perihal</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="text" required="true"  placeholder="perihal" name="perihal" class="form-control col-md-7 col-xs-12">
+										<input type="text" required="true"  placeholder="perihal" name="perihal" class="form-control col-md-7 col-xs-12" value="<?=$meetings_item['perihal']?>">
 									</div>
 								</div>
 
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Ruangan</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<select id="ruangan" name="ruangan" class="form-control" required>
-											<?php foreach($ruangans as $ruangan_item):?>
-												<option value="<?=$ruangan_item['id_ruangan']?>"><?=$ruangan_item['nama_ruangan'];?></option>
-											<?php endforeach ?>
+										<select id="ruangan" name="ruangan"  class="form-control" required>
+										<option value="<?=$meetings_item['ruangan_meeting']?>"><?=$meetings_item['nama_ruangan']?></option>
+										<?php foreach($ruangans as $ruangan_item):?>
+											<option value="<?=$ruangan_item['id_ruangan']?>"><?=$ruangan_item['nama_ruangan']?></option>
+										<?php endforeach?>
 										</select>
 									</div>
 								</div>
@@ -44,46 +45,38 @@
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Tanggal</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="date" required="true"  placeholder="dd/mm/yyyy" name="tanggal" class="form-control col-md-7 col-xs-12">
+										<input type="date" required="true"  placeholder="dd/mm/yyyy" value="<?=$meetings_item['tanggal']?>" name="tanggal" class="form-control col-md-7 col-xs-12">
 									</div>
 								</div>
 
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Jam mulai</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="time" placeholder="hh:mm" required="true"  name="waktu_mulai" class="form-control col-md-7 col-xs-12">
+										<input type="time" placeholder="hh:mm" value="<?=$meetings_item['waktu_mulai']?>" required="true"  name="waktu_mulai" class="form-control col-md-7 col-xs-12">
 									</div>
 								</div>
 
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Jam selesai</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="time" required="true"  placeholder="hh:mm" name="waktu_selesai" class="form-control col-md-7 col-xs-12">
-									</div>
-								</div>
-
-								<div class="form-group col-md-12">
-									<label class="control-label col-md-3 col-sm-3 col-xs-12">Peserta</label>
-									<div class="col-md-6 col-sm-6 col-xs-12">
-										<select  name="pengguna_meetings[]" class="inputpeserta" multiple="multiple" required>
-											<?php foreach($anggotas as $anggota_item):?>
-												<option class="form-control col-md-7 col-xs-12" value="<?=$anggota_item['id_anggota']?>"><?=$anggota_item['nama']?> - <?=$anggota_item['nama_divisi']?></option>
-											<?php endforeach?>
-										</select>
+										<input type="time" value="<?=$meetings_item['waktu_selesai']?>" required="true"  placeholder="hh:mm" name="waktu_selesai" class="form-control col-md-7 col-xs-12">
 									</div>
 								</div>
 
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">Estimasi peserta</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="number"  required="true" placeholder="estimasi peserta" name="estimasi_peserta" class="form-control col-md-7 col-xs-12">
+										<input type="number"  required="true" value="<?=$meetings_item['estimasi_peserta']?>" placeholder="estimasi peserta" name="estimasi_peserta" class="form-control col-md-7 col-xs-12">
 									</div>
 								</div>
 								<div class="form-group col-md-12">
 									<label class="control-label col-md-3 col-sm-3 col-xs-12">PIC</label>
 									<div class="col-md-6 col-sm-6 col-xs-12">
-										<input type="text" placeholder="PIC" name="pic"  required="true" class="form-control col-md-7 col-xs-12">
+										<input type="text" placeholder="PIC" value="<?=$meetings_item['PIC']?>" name="pic"  required="true" class="form-control col-md-7 col-xs-12">
 									</div>
+								</div>
+								<div>
+									<input type="hidden" name="id" value="<?=$meetings_item['id_meeting']?>">
 								</div>
 								
 							</div>
